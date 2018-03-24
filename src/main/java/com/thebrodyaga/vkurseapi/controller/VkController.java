4f470d82fd44205fe598574eb6537a0362f69fa1
WILDBERRIES\Emelyanov.n4 <Emelyanov.n4@wildberries.ru>
2018-03-24 15:50:24 +0300

    searchGroups, getGroupsById

--- src/main/java/com/thebrodyaga/vkurseapi/controller/VkController.java
+++ src/main/java/com/thebrodyaga/vkurseapi/controller/VkController.java
@@ -1,8 +1,16 @@
 package com.thebrodyaga.vkurseapi.controller;
 
+import com.thebrodyaga.vkurseapi.Application;
 import com.thebrodyaga.vkurseapi.model.GetVkWallBody;
 import com.thebrodyaga.vkurseapi.model.VkWall;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.objects.groups.GroupFull;
+import com.vk.api.sdk.objects.groups.responses.SearchResponse;
+import com.vk.api.sdk.queries.groups.GroupsGetByIdQuery;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/vk")
@@ -28,5 +36,23 @@ public class VkController {
     @PostMapping("/newWall")
     public VkWall getNewWall(@RequestBody GetVkWallBody newWallBody) {
         return new VkWall().getNewWall(newWallBody);
+    }
+
+    @GetMapping("/searchGroups")
+    public SearchResponse searchGroups(@RequestParam() String q,
+                                       @RequestParam(defaultValue = "0") Integer offset) throws ClientException, ApiException {
+        return Application.getVk().groups().search(Application.getUserActor(), q)
+                .offset(offset).execute();
+    }
+
+    @GetMapping("/getGroupsById")
+    public List<GroupFull> getGroupsById(@RequestParam(required = false) String groupId,
+                                         @RequestParam(required = false) String... groupIds) throws ClientException, ApiException {
+        GroupsGetByIdQuery query = Application.getVk().groups().getById(Application.getServiceActor());
+        if (groupId != null)
+            query.groupId(groupId);
+        if (groupIds != null)
+            query.groupIds(groupIds);
+        return query.execute();
     }
 }
